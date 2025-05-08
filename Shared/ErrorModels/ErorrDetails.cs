4ed51cd1daedb6c4972f91685e66450c610fb1c7
Ahmed Amir <150551966+ahhmedamir@users.noreply.github.com>
2025-05-08 21:53:07 +0300

    Add files via upload

---
+++ Shared/ErrorModels/ErorrDetails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Shared.ErrorModels
+{
+    public class ErorrDetails
+    {
+         public int StutusCode { get; set; }
+         public string ErrorMessage { get; set; }
+         public IEnumerable<string>?Errors { get; set; }
+        public override string ToString()
+      => JsonSerializer.Serialize(this);
+    }
+}
---
+++ Shared/ErrorModels/ValidationErrorResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.ErrorModels
+{
+    public class ValidationErrorResponse
+    {
+         public int StatusCode { get; set; }
+         public string ErrorMessage { get; set; }
+         public IEnumerable<ValidiationError> Errors { get; set; }
+    }
+     public class ValidiationError
+    {
+public string Filed { get; set; }
+         public IEnumerable<string> Errors { get; set; }
+    }
+}
